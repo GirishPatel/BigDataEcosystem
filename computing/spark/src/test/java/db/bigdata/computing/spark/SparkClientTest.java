bff55e8715e630e222729e68292a794d27f2d6e8
pankaj birat <pankajbirat@gmail.com>
2017-12-23 19:53:23 +0530

    code refactor

--- computing/spark/src/test/java/db/bigdata/computing/spark/SparkClientTest.java
+++ computing/spark/src/test/java/db/bigdata/computing/spark/SparkClientTest.java
@@ -49,7 +49,6 @@ public class SparkClientTest {
 
         for(Map.Entry<String,Integer> entry: map.entrySet()) {
             System.out.println(entry.getKey() + ":" + entry.getValue());
-
         }
     }
 
